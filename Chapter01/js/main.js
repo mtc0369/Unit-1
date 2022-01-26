f862e0958d11137ef4df6388818102278c575792
mtc0369 <mtconnolly3@wisc.edu>
2022-01-26 15:09:30 -0600

    Added files to repository

---
+++ Chapter01/js/main.js
@@ -0,0 +1,5 @@
+/* text/javascript by Michael Connolly, 2022 */
+var mydiv = document.getElementById("mydiv");
+            mydiv.addEventListener("click", function(){
+                alert("Hello World!");
+            });
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Unit-1
- 
+# Web Mapping - Unit 1
+Repository for Unit 1 of the Web Mapping Workbook.
