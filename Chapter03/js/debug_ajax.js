5d10b28d8799e80f74123a5c8bba77553cd3772d
mtc0369 <mtconnolly3@wisc.edu>
2022-02-14 10:16:14 -0600

    Chapter 3 update
    finished debugging the debug_ajax.js script.

--- Chapter03/js/debug_ajax.js
+++ Chapter03/js/debug_ajax.js
@@ -1,5 +1,121 @@
+//Chapter 2 Code
+//initialize / call addColumns & addEvents functions when the script loads
+function initialize(){
+    addColumns();
+	addEvents();
+};
+//function for setting up table and array
+function addColumns(cityPop){
+    //cityPop array with 4 objects
+	var cityPop = [
+		{city: 'Madison',
+		population: 233209},
+		{city: 'Milwaukee',
+		population: 594833},
+		{city: 'Green Bay',
+		population: 104057},
+		{city: 'Superior',
+		population: 27244}
+	];
+	//create a table element
+	var table = document.createElement("table");
+
+	//create a header row
+	var headerRow = document.createElement("tr");
+
+	//add the header row
+	table.appendChild(headerRow);
+
+	//loop to add a new row for each city
+	cityPop.forEach(function(i){
+		//create table rows
+		let tr = document.createElement("tr");
+		//variable for city data to iterate through array
+		let city = document.createElement("td");
+		city.innerHTML = i.city;
+		tr.appendChild(city);
+		//variable for population data to iterate through array
+		let pop = document.createElement("td");
+		pop.innerHTML = i.population;
+		tr.appendChild(pop);
+		//append objects from array to the table
+		table.appendChild(tr);
+	});
+
+	//querySelector linking javaScript table to index.html
+	var myDiv = document.querySelector("#myDiv");
+	myDiv.appendChild(table);
+	
+	//querySelector for all rows in table with local function containing two parameters; iterate through array
+	document.querySelectorAll("tr").forEach(function(row, i){
+		//conditional statement, setting iterator to 0
+		if (i == 0){
+			//adds header labels for City, Population, and City Size when condition above is true	
+			row.insertAdjacentHTML('beforeend', '<th>City</th><th>Population</th><th>City Size</th>');
+			//conditional statement to populate all other rows in City Size column
+			} else {
+				//creation of citySize variable under else condition
+				var citySize;
+				//conditional statement labeling small cities with a pop less than 100000
+				if (cityPop[i-1].population < 100000){
+					citySize = 'Small';
+				//conditional statement labeling medium cities with a pop less than 100000
+				} else if (cityPop[i-1].population < 500000){
+					citySize = 'Medium';
+				//conditional statement labeling large cities with a pop less than 100000
+				} else {
+					citySize = 'Large';					
+				};
+				//populates city sizes in the table column, table data html elements
+				row.insertAdjacentHTML('beforeend','<td>' + citySize + '</td>');
+			};
+    });
+};
+//fuction to add museover event with style/color
+function addEvents(){
+	//adding mouseover function to the table
+	document.querySelector("table").addEventListener("mouseover", function(){
+		//setting variable for color to equal rgb, on console will appear as rgb(#,#,#)
+		var color = "rgb(";
+		//for-loop to iterate through random rgb numbers from indices 0-3
+		for (var i=0; i<3; i++){
+			//rounds and multiplies the random numbers generated above by 255
+			random = Math.round(Math.random() * 255);
+			//reassigning color varible to include the random math
+			color += random;
+			//conditional statement inserting a comma between random rgb numbers generated 
+			if (i<2){
+				color += ",";
+			//completes the random color assignment by closing the ()
+			} else {
+				color += ")"; 
+			//logging color variable to console to see randomly generated rgb
+			//console.log(color);
+		};
+		//adds the random color assignments to the table in mouseover function
+		document.querySelector("table").style.color = color;
+	};
+	});
+	//nested function for a pop-up when the table is clicked
+	function clickme(){
+		//alert to be displayed on pop-up when table is clicked
+		alert('Hey, you clicked me!');
+	};
+	//adding the click event to the table to execute clickme function
+	document.querySelector("table").addEventListener("click", clickme);
+	//setting font size and alignment for table
+	document.querySelector('#myDiv').style.fontSize = '1em';
+    document.querySelector('#myDiv').style.textAlign = 'left';
+};
+//initialize, load script with addEventListener().
+document.addEventListener('DOMContentLoaded', initialize);
+
+
+//Chapter 3 Code
 function debugCallback(response){
-	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(mydata))
+	mydata = response
+	document.querySelector("#myDiv").insertAdjacentHTML('beforeend', '<br>GeoJSON data:<br> ' + JSON.stringify(mydata))
+	
 };
 
 function debugAjax(){
@@ -9,12 +125,16 @@ function debugAjax(){
 	fetch("data/MegaCities.geojson")
 		.then(function(response){
 			return response.json();
+
 		})
 		.then(function(response){
+			mydata = response
 			debugCallback(mydata);
+			//console.log(mydata)
 		})
 
-	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', '<br>GeoJSON data:<br>' + JSON.stringify(mydata))
+	//document.querySelector("#myDiv").insertAdjacentHTML('beforeend', '<br>GeoJSON data:<br> ' + JSON.stringify(mydata))
 };
 
-document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(mydata))
+//document.querySelector("#myDiv").insertAdjacentHTML('beforeend', '<br>GeoJSON data:<br> ' + JSON.stringify(mydata))
+document.addEventListener('DOMContentLoaded',debugAjax)
--- Chapter03/js/main.js
+++ Chapter03/js/main.js
@@ -69,23 +69,86 @@ function callback(response){
 window.onload = jsAjax();*/
 
 //Example 2.8
-function jsAjax(){
+/*function jsAjax(){
     //use fetch to retrieve data directly without using request and init variables
     fetch('data/MegaCities.geojson')
         .then(function(response){
-            return response.json();//combines the conversion function with .then using an anaonymous function 
+            return response.json();//combines the conversion function with .then using an anonymous function 
         })
         .then(callback)
-};
-/*//define the conversion callback function
+};*/
+/*//define the conversion callback function, replaced by anonymous function above
 function conversion(response){
     //convert data to usable form
     return response.json();
 };*/
-//define the callback function
+/*//define the callback function
 function callback(response){
     //tasks using the data go here
     console.log(response)
 };
 
-window.onload = jsAjax();
+window.onload = jsAjax();*/
+
+/*//Example 3.1 and 3.2: viewing fetched data without converting and print attempt with .json(). 
+function jsAjax(){
+    fetch('data/MegaCities.geojson')
+        .then(callback) 
+};
+
+function callback(response){
+    console.log(response.json())//Using .json() does print file to console opposed to generic response log.
+}
+
+window.onload = jsAjax();*/
+
+/*//Example 3.3: Correctly accessing the response using a callback() function.
+//define fetch request
+function jsAjax(){
+    //basic fetch
+    fetch('data/MegaCities.geojson')
+        .then(function(response){
+            return response.json();
+        }) 
+        .then(callback) 
+};
+
+//define callback function; Example 3.4: calling nextFunction() within the callback() function.
+function callback(response){
+    //tasks using the data go here
+    //console.log(response);
+
+    var myData = response;
+
+    //pass data to another function
+    nextFunction(myData);
+};
+function nextFunction(data){
+    console.log(data); //contains response data held by myData in callback
+
+};
+
+window.onload = jsAjax();*/
+
+//Example 3.5: An anonymous callback function 
+function jsAjax(){
+    //define a variable to hold the data
+    var myData;
+
+    //basic fetch
+    fetch('data/MegaCities.geojson')
+        .then(function(response){
+            return response.json();
+        }) 
+        .then(function(response){
+            myData = response;
+
+            //checking the data
+            console.log(myData);
+        }) 
+
+    //check the data
+    console.log(myData);
+};
+
+document.addEventListener('DOMContentLoaded',jsAjax)
